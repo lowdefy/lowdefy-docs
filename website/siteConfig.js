630a27d2a22a8ec2d7a138c4e1fbfc5247460c96
Sam Tolmay <samtolmay@gmail.com>
2019-09-25 11:45:34 +0200

    styling

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,18 +10,11 @@
 
 // List of projects/orgs using your project for the users page.
 const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+
 ];
 
 const siteConfig = {
-  title: 'Lowdefy', // Title for your website.
+  title: '', // Title for your website.
   tagline: 'Build stuff fast',
   url: 'https://www.lowdefy.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
@@ -38,17 +31,16 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: 'introduction', label: 'Docs' },
-    { page: 'help', label: 'Help' },
+    { doc: 'introduction', label: 'Docs' }
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+  headerIcon: 'img/box_white_250.png',
+  footerIcon: 'img/color_bg_100.png',
+  favicon: 'img/color_32.png',
 
   /* Colors for website */
   colors: {
